fcd28874f2da1a8881b90f6086b8c802c16e0727
Enrique Smith Dean <enriquesmith111@gmail.com>
2023-08-19 14:57:28 +0100

    added ratings db to yacht show pages

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -7,6 +7,7 @@ class YachtsController < ApplicationController
 
   def show
     @yacht = Yacht.find(params[:id])
+    @reviews = @yacht.reviews
   end
 
   def new
--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,4 +1,5 @@
 class Yacht < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews
 end
