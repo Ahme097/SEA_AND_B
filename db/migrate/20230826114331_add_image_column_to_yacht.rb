0516a84601d992ea327352e2e917fe106d9fd1e1
Enrique Smith Dean <enriquesmith111@gmail.com>
2023-09-02 15:14:40 +0100

    added images to groups and avatar

--- db/migrate/20230826114331_add_image_column_to_yacht.rb
+++
@@ -1,6 +0,0 @@
-class AddImageColumnToYacht < ActiveRecord::Migration[7.0]
-  def change
-    add_column :yachts, :yacht_interior_image, :string
-    add_column :yachts, :yacht_cabin_image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_114331) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_121238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,9 +88,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_114331) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.bigint "user_id"
     t.string "yacht_interior_image"
     t.string "yacht_cabin_image"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 
